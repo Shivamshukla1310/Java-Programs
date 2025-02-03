994597a0563b597b5be2cb74cfc184c2d62f24ed
Shivam Shukla <shukla.shivamv@gmail.com>
2025-02-04 01:29:14 +0530

    Some Prg Added

---
+++ Length_Program.java
@@ -0,0 +1,17 @@
+// To Count The Given String for its Length
+
+class Length_Program
+{
+    public static void main(String args[])
+    {
+        String s1 = "Sunstone ";
+        String s2 = " ADYPU";
+        int len = s1.length();
+        String concati = s1.concat(s2);
+        System.out.println(concati);
+        String trim = concati.trim();
+        System.out.println(len);
+        System.out.println(trim);
+
+    }
+}
