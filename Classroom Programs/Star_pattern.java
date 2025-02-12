2d517fca52ef1d93ca11844f48725e8ab446817b
Shivam Shukla <shukla.shivamv@gmail.com>
2025-02-12 14:46:02 +0530

    Star_pattern Uploaded

---
+++ Classroom Programs/Star_pattern.java
@@ -0,0 +1,24 @@
+/* Display Following Pattern
+*
+**
+***
+****
+*****
+ */
+import java.util.Scanner;
+class Star_pattern
+{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter Number OF Lines: ");
+        int n = s.nextInt();
+        for(int i = 0; i <= n-1; i++)
+        {
+            for(int j = 0; j <=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
