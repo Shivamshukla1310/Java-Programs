cd0eb5d46f171f14f2669c79fb9ce1fe9e02e585
Shivam Shukla <shukla.shivamv@gmail.com>
2025-02-17 10:54:13 +0530

    prg updated

---
+++ exam.java
@@ -0,0 +1,12 @@
+class exam{
+    public static void main(String args[]){
+    // short a = 234;
+    // int b = 254;
+    // int c = 0;
+    // c = a * 10 + b - a;
+    // System.out.printf("C: %d",c);
+    }
+    public static void main(String args){
+        
+    }
+}
