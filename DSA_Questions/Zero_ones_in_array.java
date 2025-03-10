8f58a942ef92ff671acfe667c4622a2c8bdc2353
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-10 16:14:23 +0530

    Programs Updated

--- DSA_Questions/Zero_ones_in_array.java
+++ DSA_Questions/Zero_ones_in_array.java
@@ -1,31 +1,31 @@
-public class Zero_Ones_in_array{
-    staticvoid segregate0and1(int arr[],int n){
+class Zero_ones_in_array {
+    static void segregate0and1(int arr[], int n) {
         int count = 0;
-        for(int i = 0; i < 0; i++){
-            if(arr[i] == 0){
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
                 count++;
             }
         }
-        for(int i = 0; i < count; i++){
+        for (int i = 0; i < count; i++) {
             arr[i] = 0;
         }
-        for(int i = 0; i < count; i++){
+        for (int i = count; i < n; i++) {
             arr[i] = 1;
         }
     }
 
-    static void print(int arr[], int n){
-        System.out.println("Array after Seggration");
-        for(int i = 0; i < n; i++){
+    static void print(int arr[], int n) {
+        System.out.println("Array after Segregation");
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
     }
 
-    public static void main(String args[]){
-        int arr[] = {0,1,0,1,1,1};
+    public static void main(String args[]) {
+        int arr[] = {0, 1, 0, 1, 1, 1};
         int n = arr.length;
 
-        segregate0and1(arr,n);
-        print(arr,n);
+        segregate0and1(arr, n);
+        print(arr, n);
     }
 }
