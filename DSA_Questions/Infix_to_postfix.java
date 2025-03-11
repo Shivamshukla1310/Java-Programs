775d1e3f79d37d862b2a312c4f60d80c4df46213
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-11 10:49:55 +0530

    Programs Updated

---
+++ DSA_Questions/Infix_to_postfix.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class Infix_to_postfix{
+    private static int precedence(chr op){
+        switch(op){
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+            default:
+                return -1;
+        }
+    }
+
+    public static String Infix_to_postfix(String infix){
+        StrinfBuilder postfix = new StrinfBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; j < infix.length(); i++){
+            char ch = infix.charAt(i);
+        }
+    }
+}
