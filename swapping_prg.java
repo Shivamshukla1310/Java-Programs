4eefaebcc31096e6fa1afdedd630a3cc891e39a9
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-16 11:55:51 +0530

    Programs Updated

--- swapping_prg.java
+++ swapping_prg.java
@@ -1,9 +1,9 @@
+// Swapping in one line
 class swapping_prg{
     public static void main(String args[]){
         int a = 10;
         int b = 20;
         a = a + b - (b = a);
-        
         System.out.println(a);
         System.out.println(b);
     
