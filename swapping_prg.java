4b6dd746284a9304cb636f961e1f4fd47baeb783
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-16 11:52:35 +0530

    Program Updated

---
+++ swapping_prg.java
@@ -0,0 +1,11 @@
+class swapping_prg{
+    public static void main(String args[]){
+        int a = 10;
+        int b = 20;
+        a = a + b - (b = a);
+        
+        System.out.println(a);
+        System.out.println(b);
+    
+    }
+}
