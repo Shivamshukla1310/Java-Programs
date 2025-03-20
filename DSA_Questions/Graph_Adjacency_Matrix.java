cbab5e10b9abd5ebf97229e3b057ebaaa76d7580
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-20 23:25:20 +0530

    Programs updated

---
+++ DSA_Questions/Graph_Adjacency_Matrix.java
@@ -0,0 +1,23 @@
+/*You need to implement an undirected graph using an adjacency matrix. The program should allow the user to:
+
+Input the number of vertices (N) and number of edges (E).
+Enter E pairs of vertices (U, V) that represent edges between vertex U and V.
+Print the adjacency matrix representation of the graph.
+Input Format:
+An integer N → Number of vertices.
+An integer E → Number of edges.
+E lines, each containing two integers U V representing an edge between vertex U and vertex V.
+Output Format:
+The adjacency matrix representation of the graph.
+Constraints:
+1 ≤ N ≤ 100
+0 ≤ E ≤ N*(N-1)/2 (No self-loops and no parallel edges)
+1 ≤ U, V ≤ N
+For example:
+
+Input	Result
+4 3     0 1 0 1
+1 2     1 0 1 0
+2 3     0 1 0 0
+4 1     1 0 0 0
+*/
