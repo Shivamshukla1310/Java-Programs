1bed1e44abadc5ebe2e1054ae1726098dd2d9236
Shivam Shukla <shukla.shivamv@gmail.com>
2025-04-15 15:26:03 +0530

    Square root for number without built in

---
+++ Square_Root.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Square_Root{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        double a = sc.nextInt();
+        System.out.print("Square Root: " + (double)(a) / );
+
+    }
+}
