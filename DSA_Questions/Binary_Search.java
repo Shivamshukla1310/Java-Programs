d313786b9e57a554821f5e0aa0745a003d096230
Shivam Shukla <shukla.shivamv@gmail.com>
2025-04-24 15:10:50 +0530

    Programs Updated

---
+++ DSA_Questions/Binary_Search.java
@@ -0,0 +1,17 @@
+// 
+
+import java.util.*;
+
+class Binary_Search{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new arr[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int 
+    }
+}
